a5392646c52aa758c35e371b9e5a6c72c743bb22
PhucThaii <phucthai2943@gmail.com>
2024-04-10 18:08:43 +0700

    test 2

--- test_patch/gametest/AiVirtualMouseProject.py
+++ test_patch/gametest/AiVirtualMouseProject.py
@@ -3,11 +3,12 @@ import numpy as np
 import HandTrackingModule as htm
 import time
 import autopy
+from autopy import mouse
 import pyautogui
 
 ##########################
 wCam, hCam = 640, 480
-frameR = 100 # Frame Reduction
+frameR = 75  # Frame Reduction
 smoothening = 7
 #########################
 
@@ -15,12 +16,13 @@ pTime = 0
 plocX, plocY = 0, 0
 clocX, clocY = 0, 0
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 cap.set(3, wCam)
 cap.set(4, hCam)
 detector = htm.handDetector(maxHands=1)
 wScr, hScr = autopy.screen.size()
-#print(wScr, hScr)
+
+# print(wScr, hScr)
 
 while True:
     # 1. Find hand Landmarks
@@ -32,12 +34,12 @@ while True:
         x1, y1 = lmList[8][1:]
         x2, y2 = lmList[12][1:]
         # print(x1, y1, x2, y2)
-    
+
         # 3. Check which fingers are up
         fingers = detector.fingersUp()
         # print(fingers)
-        cv2.rectangle(img, (frameR, frameR), (wCam - frameR, hCam - frameR),
-        (255, 0, 255), 2)
+        #cv2.rectangle(img, (frameR, frameR), (wCam - frameR, hCam - frameR), (255, 0, 255), 2)
+
         # 4. Only Index Finger : Moving Mode
         if fingers[1] == 1 and fingers[2] == 0:
             # 5. Convert Coordinates
@@ -46,37 +48,33 @@ while True:
             # 6. Smoothen Values
             clocX = plocX + (x3 - plocX) / smoothening
             clocY = plocY + (y3 - plocY) / smoothening
-    
+
             # 7. Move Mouse
             autopy.mouse.move(wScr - clocX, clocY)
-            #reverse Mouse (wScr - clocX)
+
+            autopy.mouse.toggle(down=True)
+            # reverse Mouse (wScr - clocX)
             cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
             plocX, plocY = clocX, clocY
-        
+
         # 8. Both Index and middle fingers are up : Clicking Mode
         if fingers[1] == 1 and fingers[2] == 1:
             # 9. Find distance between fingers
             length, img, lineInfo = detector.findDistance(8, 12, img)
             print(length)
             # 10. Click mouse if distance short
-            if length < 40:
-                cv2.circle(img, (lineInfo[4], lineInfo[5]), 15, (0, 255, 0), cv2.FILLED)               
-                autopy.mouse.click()
-    
+            if length < 50:
+                cv2.circle(img, (lineInfo[4], lineInfo[5]), 15, (0, 255, 0), cv2.FILLED)
+                autopy.mouse.click(autopy.mouse.Button.RIGHT)
+
     # 11. Frame Rate
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
     cv2.putText(img, str(int(fps)), (20, 50), cv2.FONT_HERSHEY_PLAIN, 3,
-    (255, 0, 0), 3)
+                (255, 0, 0), 3)
     # 12. Display
     cv2.imshow("Image", img)
-    key=cv2.waitKey(1)
-    if key ==27:
+    key = cv2.waitKey(1)
+    if key == 27:
         break
-
-    
-    
-  
-            
-            
